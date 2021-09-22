b7d106c7d350c01f4260a3f1fc0caa994f0c06d9
მურად ჯანაშვილი <murad.janashvili617@ens.tsu.edu.ge>
2021-09-22 12:11:27 +0400

    added repo,must change

---
+++ PontiApp.Images.Repository/ImageRepository.cs
@@ -0,0 +1,46 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontiApp.Images.Repository
+{
+    public class ImageRepository : IImageRepository
+    {
+        public MongoClient client { get; set; }
+        public IMongoDatabase DB { get; set; }
+        public GridFSBucket FS { get; set; }
+        public IHttpClientFactory ClientFac { get; set; }
+
+        public ImageRepository(MongoClient _client,IHttpClientFactory _ClientFac)
+        {
+            client = _client;
+            DB = client.GetDatabase("DBname");
+            FS = new GridFSBucket(DB);
+            
+            
+        }
+        public async Task DeleteImage(string FileID)=>await FS.DeleteAsync(FileID);
+
+        public async Task<byte[]> GetImage(string FileID)=> await FS.DownloadAsBytesAsync(FileID);
+
+
+        public async Task PostImage(byte[] imgData)
+        { 
+
+        }
+        
+
+        public async Task PostImage(string Uri)
+        {
+            var httpClient = ClientFac.CreateClient();
+            var imgData = await httpClient.GetByteArrayAsync(Uri);
+            await FS.UploadFromBytesAsync(new Guid().ToString(),imgData);
+        }
+    }
+}
---
+++ PontiApp.Images.Services/IImageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontiApp.Images.Services
+{
+    public interface IImageService
+    {
+        Task<byte[]> GetImage(string FileID);
+        Task DeleteImage(string FileID);
+        Task PostImage(string url);
+        Task PostImage(byte[] imgData);
+    }
+}
