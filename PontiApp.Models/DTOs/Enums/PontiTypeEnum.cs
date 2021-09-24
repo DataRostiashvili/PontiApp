bfc42dc8966d2980504ac4f819399a1b823abf22
Aleksandre Naneishvili <aleksandrenaneishvili@gmail.com>
2021-09-24 11:14:26 +0400

    addedSearchDTOs

---
+++ PontiApp.Models/DTOs/Enums/PontiTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontiApp.Models.DTOs.Enums
+{
+    public enum PontiTypeEnum
+    {
+        Event,
+        Place
+    }
+}
---
+++ PontiApp.Models/DTOs/MyPontsFilter.cs
@@ -0,0 +1,17 @@
+﻿using PontiApp.Models.DTOs.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontiApp.Models.DTOs
+{
+    public class MyPontsFilter
+    {
+        public int QueueId { get; set; }
+        PontiTypeEnum PontiType { get; set; }
+        MyRoleEnum MyRole { get; set; }
+        
+    }
+}
