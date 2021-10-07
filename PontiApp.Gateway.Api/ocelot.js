0a94e9b5576b9731a36c8cb9fe3ef69a5c22e09c
Murad000 <59088139+Murad000@users.noreply.github.com>
2021-10-07 15:17:30 +0400

    getting changes from master

---
+++ PontiApp.Gateway.Api/ocelot.js
@@ -0,0 +1,38 @@
+﻿{
+    "$schema": "http://json.schemastore.org/launchsettings.json",
+        "iisSettings": {
+        "windowsAuthentication": false,
+            "anonymousAuthentication": true,
+                "iisExpress": {
+            "applicationUrl": "http://localhost:19940",
+                "sslPort": 44329
+        }
+    },
+    "profiles": {
+        "IIS Express": {
+            "commandName": "IISExpress",
+                "launchBrowser": true,
+                    "launchUrl": "swagger",
+                        "environmentVariables": {
+                "ASPNETCORE_ENVIRONMENT": "Development"
+            }
+        },
+        "PontiApp.EventPlace.Api": {
+            "commandName": "Project",
+                "launchBrowser": true,
+                    "launchUrl": "swagger",
+                        "environmentVariables": {
+                "ASPNETCORE_ENVIRONMENT": "Development"
+            },
+            "applicationUrl": "https://localhost:5001;http://localhost:5000",
+                "dotnetRunMessages": "true"
+        },
+        "Docker": {
+            "commandName": "Docker",
+                "launchBrowser": true,
+                    "launchUrl": "{Scheme}://{ServiceHost}:{ServicePort}/swagger",
+                        "publishAllPorts": true,
+                            "useSSL": true
+        }
+    }
+}
