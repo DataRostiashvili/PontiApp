6824ec362fc0fb9e4ac4454e88a8a99d6ddedf42
Murad000 <59088139+Murad000@users.noreply.github.com>
2021-10-18 12:41:30 +0400

    bugfixes and improvements

--- PontiApp.Utilities/RabbitMQConsts.cs
+++ PontiApp.Utilities/RabbitMQConsts.cs
@@ -10,7 +10,7 @@ namespace PontiApp.MessageSender
     {
 
 
-        public const string EXCHANGE = "PontiAppEx";
+        public const string EXCHANGE = "PontiApp";
         public const string UPDATE_ADD_Q = "Update.Add";
         public const string UPDATE_REMOVE_Q = "Update.Remove";
         public const string ADD_Q = "Add";
