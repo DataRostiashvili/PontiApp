ecb9e7a3853e1e7651998ce93e955b7ab3004400
maka dabakhishvili <maka.dabaxishvili@gmail.com>
2021-11-02 10:22:28 +0400

    Add DatabaseSeeder Functionality

--- PontiApp.Data/DatabaseSeeder/DatabaseSeeder.cs
+++ PontiApp.Data/DatabaseSeeder/DatabaseSeeder.cs
@@ -1,10 +1,50 @@
+using PontiApp.Data.DbContexts;
+using System.Linq;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+
 namespace PontiApp.Data.DatabaseSeeder
 {
     public class DatabaseSeeder
     {
-        public DatabaseSeeder()
+        private readonly ApplicationDbContext _appDbContext;
+        public DatabaseSeeder(ApplicationDbContext appDbContext)
         {
-            
+            _appDbContext = appDbContext;
+        }
+
+        public void SeedDatabase()
+        {
+            FillUsersTable();
+            FillPlacesTable();
+            FillEventsTable();
+        }
+
+        private void FillUsersTable()
+        {
+            if (!_appDbContext.Users.Any())
+            {
+                var userSqlData = File.ReadAllText(@"..\PontiApp.Data\DatabaseSeeder\InsertUsers.sql");
+                _appDbContext.Database.ExecuteSqlRaw(userSqlData);
+            }
+        }
+
+        private void FillPlacesTable()
+        {
+            if (!_appDbContext.Places.Any())
+            {
+                var placesSqlData = File.ReadAllText(@"..\PontiApp.Data\DatabaseSeeder\InsertPlaces.sql");
+                _appDbContext.Database.ExecuteSqlRaw(placesSqlData);
+            }
+        }
+
+        private void FillEventsTable()
+        {
+            if (!_appDbContext.Events.Any())
+            {
+                var eventsSqlData = File.ReadAllText(@"..\PontiApp.Data\DatabaseSeeder\InsertEvents.sql");
+                _appDbContext.Database.ExecuteSqlRaw(eventsSqlData);
+            }
         }
     }
 }
---
+++ TestingAPI/Controllers/DatabaseSeederController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PontiApp.Data.DatabaseSeeder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatabaseSeederController :ControllerBase
+    {
+        private readonly DatabaseSeeder _seeder;
+        public DatabaseSeederController(DatabaseSeeder seeder)
+        {
+            _seeder = seeder;
+        }
+
+        [HttpGet]
+        public IActionResult SeedDatabase()
+        {
+            _seeder.SeedDatabase();
+            return Ok();
+        } 
+
+    }
+}
