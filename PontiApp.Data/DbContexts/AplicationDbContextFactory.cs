eb31db6fc24e85d485ec886cb269319c2ca79c6c
Vinby <d4v1t.r0st@gmail.com>
2021-11-08 20:22:24 +0400

    add design time context factory

---
+++ PontiApp.Data/DbContexts/AplicationDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace PontiApp.Data.DbContexts
+{
+
+
+    public class AplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseNpgsql("User ID=postgres;Password=mysecretpassword;Host=localhost;Port=5432;Database=postgres;");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
