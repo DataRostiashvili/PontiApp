6de1ed2382c84340bb37466d659d8bb7d2975c76
Data Rostiashvili <48126197+Vinby@users.noreply.github.com>
2021-11-09 11:35:14 +0400

    add cors policy

--- PontiApp.EventPlace.Api/Startup.cs
+++ PontiApp.EventPlace.Api/Startup.cs
@@ -46,6 +46,16 @@ namespace PontiApp.EventPlace.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                  "CorsPolicy",
+                  builder => builder
+                  .AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader());
+            });
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -94,14 +104,20 @@ namespace PontiApp.EventPlace.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("CorsPolicy");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PontiApp.Auth v1"));
             }
+
             app.UseHttpsRedirection();
             app.UseRouting();
+
+           
+
 
             app.UseAuthentication();
 
