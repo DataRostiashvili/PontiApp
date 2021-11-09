f7f4089f6d2ed47e857099c3396a8b471b46cfbd
SandroNanesha <56513599+SandroNanesha@users.noreply.github.com>
2021-11-09 17:14:05 +0400

    refactoredDTOs

--- PontiApp.Models/DTOs/PlaceListingResponseDTO.cs
+++ PontiApp.Models/DTOs/PlaceListingResponseDTO.cs
@@ -10,6 +10,10 @@ namespace PontiApp.Models.DTOs
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public UserListingDTO UserEntity { get; set; }
         public List<WeekScheduleDTO> WeekSchedules { get; set; }
         public int AverageRanking { get; set; }
     }
