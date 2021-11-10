d8c41c02aec8096a11cf0c6604b2676877aa5738
Murad000 <59088139+Murad000@users.noreply.github.com>
2021-11-10 15:42:55 +0400

    Working on validators

---
+++ PontiApp.FluentValidator/EventCategoryDTOValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using PontiApp.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontiApp.FluentValidator
+{
+    public class EventCategoryDTOValidator:AbstractValidator<EventCategoryDTO>
+    {
+        public EventCategoryDTOValidator()
+        {
+            RuleFor(x => x.EventEntityId).NotNull();
+            RuleFor(x => x.CategoryEntityId).NotNull();
+        }
+    }
+}
---
+++ PontiApp.FluentValidator/EventHostRequestDTOValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontiApp.FluentValidator
+{
+    class EventHostRequestDTOValidator
+    {
+    }
+}
