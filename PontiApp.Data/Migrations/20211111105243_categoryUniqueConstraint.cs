c647aebfb1f9f4bc19a2929cc71c305d8fcd158a
Data Rostiashvili <48126197+Vinby@users.noreply.github.com>
2021-11-11 14:54:00 +0400

    unique constraint for category table

---
+++ PontiApp.Data/Migrations/20211111105243_categoryUniqueConstraint.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PontiApp.Data.Migrations
+{
+    public partial class categoryUniqueConstraint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Category",
+                table: "Categories",
+                column: "Category",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_Category",
+                table: "Categories");
+        }
+    }
+}
