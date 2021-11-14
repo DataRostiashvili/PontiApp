418776975d4eee3e2900ba2fc58b8199adec4309
Data Rostiashvili <48126197+Vinby@users.noreply.github.com>
2021-11-14 16:03:45 +0400

    added docker-compose-minanal.yaml for running minimum set of containers in order to start application locally

---
+++ PontiApp.Models/Request/PlaceCategoryRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontiApp.Models.Request
+{
+    public class PlaceCategoryRequest
+    {
+        public string Category { get; set; }
+        public int PlaceEntityId { get; set; }
+
+     
+    }
+}
+}
---
+++ PontiApp.Models/Response/PlaceCategoryResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontiApp.Models.Response
+{
+    public class PlaceCategoryResponse
+    {
+        public int CategoryEntityId { get; set; }
+        public string Category { get; set; }
+        public int PlaceEntityId { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
+}
